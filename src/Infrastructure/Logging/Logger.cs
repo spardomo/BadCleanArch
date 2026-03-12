b8d36cbf434116ff0d1668bb251099654883547a
xardo <pardomoyasegio@gmail.com>
2026-03-12 10:16:21 -0500

    correcciones logger y domain referenciando infraestructura

--- src/Infrastructure/Logging/Logger.cs
+++ src/Infrastructure/Logging/Logger.cs
@@ -1,19 +1,19 @@
 using System;
 
-namespace Infrastructure.Logging;
-
-public static class Logger
-{
-    public static bool Enabled = true;
-
-    public static void Log(string message)
+namespace Infrastructure.Logging { 
+    public static class Logger
     {
-        if (!Enabled) return;
-        Console.WriteLine("[LOG] " + DateTime.Now + " - " + message);
-    }
+        public static bool Enabled = true;
 
-    public static void Try(Action a)
-    {
-        try { a(); } catch { }
+        public static void Log(string message)
+        {
+            if (!Enabled) return;
+            Console.WriteLine("[LOG] " + DateTime.Now + " - " + message);
+        }
+
+        public static void Try(Action a)
+        {
+            try { a(); } catch { }
+        }
     }
 }
