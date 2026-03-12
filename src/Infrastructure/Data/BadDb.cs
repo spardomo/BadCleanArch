dbcde42614af699c6e16cabea54888cfe0ece79f
xardo <pardomoyasegio@gmail.com>
2026-03-12 08:05:07 -0500

    correccion de referencias

--- src/Infrastructure/Data/BadDb.cs
+++ src/Infrastructure/Data/BadDb.cs
@@ -2,28 +2,27 @@ using System;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace Infrastructure.Data;
-
-using System.Data;
-using System.Data.SqlClient;
-
-public static class BadDb
-{
-    public static string ConnectionString = "Server=localhost;Database=master;User Id=sa;Password=SuperSecret123!;TrustServerCertificate=True";
-
-
-    public static int ExecuteNonQueryUnsafe(string sql)
+namespace Infrastructure.Data
+{ 
+    public static class BadDb
     {
-        var conn = new SqlConnection(ConnectionString);
-        var cmd = new SqlCommand(sql, conn);
-        conn.Open();
-        return cmd.ExecuteNonQuery();
+        public static string ConnectionString = "Server=localhost;Database=master;User Id=sa;Password=SuperSecret123!;TrustServerCertificate=True";
+
+
+        public static int ExecuteNonQueryUnsafe(string sql)
+        {
+            var conn = new SqlConnection(ConnectionString);
+            var cmd = new SqlCommand(sql, conn);
+            conn.Open();
+            return cmd.ExecuteNonQuery();
+        }
+
+        public static IDataReader ExecuteReaderUnsafe(string sql)
+        {
+            var conn = new SqlConnection(ConnectionString);
+            var cmd = new SqlCommand(sql, conn);
+            conn.Open();
+            return cmd.ExecuteReader();
+        }
     }
-
-    public static IDataReader ExecuteReaderUnsafe(string sql)
-    {
-        var conn = new SqlConnection(ConnectionString);
-        var cmd = new SqlCommand(sql, conn);
-        conn.Open();
-        return cmd.ExecuteReader(); 
 }
